c8233a08a8a393897c54116cfff6c241302f19e7
alexandrovaAY <alexandrova419@gmail.com>
2025-11-04 12:03:59 +0300

    feat:исправление замечаний  Фикстуры Pytest.Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -4,17 +4,18 @@ from playwright.sync_api import expect, Page
 @pytest.mark.regression
 @pytest.mark.courses
 def test_empty_courses_list(chromium_page_with_state: Page):
-        courses_text = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-        expect(courses_text).to_be_visible()
-        expect(courses_text).to_have_text("Courses")
+    chromium_page_with_state.goto(" https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_text = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+    expect(courses_text).to_be_visible()
+    expect(courses_text).to_have_text("Courses")
 
-        courses_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-        expect(courses_icon).to_be_visible()
+    courses_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+    expect(courses_icon).to_be_visible()
 
-        title_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-        expect(title_text).to_be_visible()
-        expect(title_text).to_have_text("There is no results")
+    title_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+    expect(title_text).to_be_visible()
+    expect(title_text).to_have_text("There is no results")
 
-        description_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-        expect(description_text).to_be_visible()
-        expect(description_text).to_have_text("Results from the load test pipeline will be displayed here")
+    description_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
+    expect(description_text).to_be_visible()
+    expect(description_text).to_have_text("Results from the load test pipeline will be displayed here")
